0541c4780310f2583e0d533c32c8079404b8870a
TheGuardianWolf <nano@pixelcollider.net>
2020-04-12 08:08:39 +1200

    Add server switch

--- src/app/header/AppBar.tsx
+++ src/app/header/AppBar.tsx
@@ -1,6 +1,9 @@
+import { ChevronLeft, Storage } from '@material-ui/icons';
 import {
   IconButton,
   AppBar as MaterialAppBar,
+  Menu,
+  MenuItem,
   Toolbar,
   Typography,
   makeStyles,
@@ -9,7 +12,7 @@ import React, { useEffect, useMemo, useState } from 'react';
 import { generateSearchQuery, getSearchParams } from 'util/search-utils';
 import { useHistory, useRouteMatch } from 'react-router-dom';
 
-import { ChevronLeft } from '@material-ui/icons';
+import { AxiosClient } from 'app/requests';
 import SearchInput from 'components/search-input/SearchInput';
 import clsx from 'clsx';
 
@@ -23,11 +26,18 @@ const useStyles = makeStyles((theme) => ({
   toolbar: {
     flexWrap: 'wrap',
   },
-  menuButton: {
+  toolbarButtonNavigateBack: {
     marginRight: theme.spacing(2),
     order: 1,
     [theme.breakpoints.down('xs')]: {
       marginRight: 0,
+    },
+  },
+  toolbarButtonServerSelect: {
+    marginLeft: 'auto',
+    order: 4,
+    [theme.breakpoints.down('xs')]: {
+      order: 3,
     },
   },
   title: {
@@ -54,18 +64,35 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const servers = [
+  {
+    client: AxiosClient.E621,
+    name: 'NSFW (e621.net)',
+  },
+  {
+    client: AxiosClient.E926,
+    name: 'SFW (e926.net)',
+  },
+];
+
 export function AppBar() {
   const classes = useStyles();
 
   const history = useHistory();
+  const match = useRouteMatch<{ client: AxiosClient | undefined }>('/:client');
 
-  const match = useRouteMatch<{ client: string }>('/:client');
+  const client = useMemo(() => match?.params.client ?? AxiosClient.E621, [
+    match,
+  ]);
 
   const tags = useMemo(() => getSearchParams(history.location.search, 'tags'), [
     history.location.search,
   ]);
 
   const [searchInput, setSearchInput] = useState('');
+  const [serverSelectAnchorEl, setServerSelectAnchorEl] = useState<
+    (EventTarget & HTMLButtonElement) | null
+  >(null);
 
   useEffect(() => {
     setSearchInput(tags.join(' '));
@@ -85,7 +112,10 @@ export function AppBar() {
             history.push(`/${match?.params.client ?? ''}`);
           }}
           edge="start"
-          className={clsx(classes.menuButton, isHome && classes.invisible)}
+          className={clsx(
+            classes.toolbarButtonNavigateBack,
+            isHome && classes.invisible
+          )}
         >
           <ChevronLeft />
         </IconButton>
@@ -106,6 +136,44 @@ export function AppBar() {
             });
           }}
         />
+        <IconButton
+          color="inherit"
+          aria-label="server select"
+          aria-controls="menu-server"
+          aria-haspopup="true"
+          onClick={(e) => setServerSelectAnchorEl(e.currentTarget)}
+          edge="start"
+          className={classes.toolbarButtonServerSelect}
+        >
+          <Storage />
+        </IconButton>
+        <Menu
+          id="menu-server"
+          anchorEl={serverSelectAnchorEl}
+          keepMounted
+          open={!!serverSelectAnchorEl}
+          onClose={() => setServerSelectAnchorEl(null)}
+        >
+          {servers.map((server) => (
+            <MenuItem
+              key={server.name}
+              selected={client === server.client}
+              onClick={() => {
+                const pathComponents = history.location.pathname.split('/');
+                if (pathComponents.length > 1) {
+                  pathComponents[1] = server.client;
+                }
+                history.push({
+                  ...history.location,
+                  pathname: pathComponents.join('/'),
+                });
+                setServerSelectAnchorEl(null);
+              }}
+            >
+              {server.name}
+            </MenuItem>
+          ))}
+        </Menu>
       </Toolbar>
     </MaterialAppBar>
   );
