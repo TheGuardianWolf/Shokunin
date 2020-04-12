b8062c6241c93630ee4d2c315f8639d3331da417
TheGuardianWolf <nano@pixelcollider.net>
2020-04-12 19:59:39 +1200

    Hard code GA

--- src/components/google-analytics/GoogleAnalytics.tsx
+++ src/components/google-analytics/GoogleAnalytics.tsx
@@ -3,16 +3,12 @@ import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 let gaInit = false;
-if (process.env.NODE_ENV === 'production') {
-  if (process.env.GOOGLE_ANALYTICS_ID) {
-    ReactGA.initialize(process.env.GOOGLE_ANALYTICS_ID ?? '');
-    gaInit = true;
-    console.info('Now tracking GA data.');
-  } else {
-    console.warn('GA tracking cannot be enabled.');
-  }
-} else {
+if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
   console.info('GA disabled as it is not a production environment');
+} else {
+  ReactGA.initialize('UA-163415862-1');
+  gaInit = true;
+  console.info('Now tracking GA data.');
 }
 
 export function GoogleAnalytics() {
